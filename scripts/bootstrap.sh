249859112c2530bd49509d70d55685e4d97b87de
Jorge Villalobos <mail@minusfive.com>
2024-08-22 15:53:26 -0400

    feat(bootstrap): run updates after install

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,14 +10,11 @@ local GITIGNORE_PATH="$DOTFILES_PATH/.gitignore"
 
 # Install or update Homebrew
 if [[ $(command -v brew) != "" ]]; then
-    echo "- Homebrew installed at $(which brew). Attempting upgrade..."
-    brew upgrade
-    if [[ $? == 0 ]]; then
-      echo "- Everything up to date"
-    fi
+    echo "- Homebrew installed at $(which brew)"
 else
     echo "- Homebrew not installed. Attempting install..."
     /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
     if [[ $? == 0 ]]; then
       echo "- Homebrew installed, adding to path..."
       (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
@@ -25,24 +22,42 @@ else
     fi
 fi
 
-# Install desired tools, apps, etc.
-if [[ ! -f $BREW_BUNDLE_PATH ]]; then
-    echo "- Brewfile not found"
-else
-    echo "- Installing Homebrew bundle"
-    brew bundle -v --file $BREW_BUNDLE_PATH
+
+# Upgrade Homebrew and install of upgrade packages and apps
+if [[ $(command -v brew) != "" ]]; then
+    # Install desired tools, apps, etc.
+    if [[ ! -f $BREW_BUNDLE_PATH ]]; then
+        echo "- Brewfile not found"
+    else
+        echo "- Installing Homebrew bundle"
+        brew bundle -v --file $BREW_BUNDLE_PATH
+    fi
+
+    if [[ $(brew commands -q | grep cu -c) == 1 ]]; then
+      echo "brew-cask-upgrade found; attempting to upgrade Homebrew, packages and apps..."
+      brew cu -a
+    else
+      echo "brew-cask-upgrade not found; attempting to upgrade Homebrew and packages..."
+      brew upgrade
+    fi
+
+    if [[ $? == 0 ]]; then
+      echo "- Everything up to date"
+    fi
 fi
 
+
 # Install OhMyZsh
-if [[ -d $HOME/.oh-my-zsh ]]; then
+if [[ -d ${ZSH:-$HOME/.oh-my-zsh} ]]; then
     echo "- OhMyZsh already installed"
 else
     echo "- Installing OhMyZsh"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+
 # Install PowerLevel10K
-if [[ -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ]]; then
+if [[ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]]; then
     echo "- PowerLevel10K already installed"
 else
     echo "- Installing PowerLevel10K"
@@ -50,8 +65,9 @@ else
       ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
+
 # Install fast-syntax-highlighting
-if [[ -d $HOME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting ]]; then
+if [[ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting ]]; then
     echo "- fast-syntax-highlighting already installed"
 else
     echo "- Installing fast-syntax-highlighting"
@@ -61,7 +77,7 @@ fi
 
 
 # Install zsh-autosuggestions
-if [[ -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]]; then
+if [[ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]]; then
     echo "- zsh-autosuggestions already installed"
 else
     echo "- Installing zsh-autosuggestions"
@@ -69,3 +85,16 @@ else
       ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
+
+# Install OhMyZsh Full-autoupdate
+if [[ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate ]]; then
+    echo "- OhMyZsh Full-autoupdate already installed"
+else
+    echo "- Installing OhMyZsh Full-autoupdate"
+    git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git \
+      ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
+fi
+
+
+# Update OhMyZsh, plugins and themes
+zsh $HOME/.oh-my-zsh/tools/upgrade.sh
