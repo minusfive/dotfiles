b535661bd95988d00bb33a2620273534c6593991
Jorge Villalobos <jorge.villalobos@autodesk.com>
2024-08-28 16:59:42 -0400

    feat(hammerspoon): Finder raise all windows

--- .hammerspoon/modules/appLauncher.lua
+++ .hammerspoon/modules/appLauncher.lua
@@ -1,4 +1,5 @@
 -- Module to open / focus applictions
+---@class AppLauncher
 local AppLauncher = {}
 
 -- Quickly open and/or focus applications
@@ -8,8 +9,10 @@ function AppLauncher:openApp(name)
 	return function()
 		hs.application.launchOrFocus(name)
 		if name == "Finder" then
+			---@type hs.application | nil
 			local app = hs.appfinder.appFromName(name)
 			if app then
+				app:selectMenuItem({ "Window", "Bring All to Front" })
 				app:activate()
 			end
 		end
