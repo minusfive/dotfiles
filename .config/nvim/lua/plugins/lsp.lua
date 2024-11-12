d618b62469b296291fa7fca59ca97481225bc5d1
Jorge Villalobos <jorge.villalobos@autodesk.com>
2024-11-12 00:39:16 -0500

    feat(nvim): add harper-ls for grammar/spelling

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,23 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    --@class PluginLspOpts
+    ---@class PluginLspOpts
     opts = {
       inlay_hints = { enabled = false },
+      servers = {
+        harper_ls = {
+          settings = {
+            ["harper-ls"] = {
+              userDictPath = "~/.config/harper/dictionaries/user.txt",
+              fileDictPath = "~/.config/harper/dictionaries/files/",
+
+              codeActions = {
+                forceStable = true,
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
