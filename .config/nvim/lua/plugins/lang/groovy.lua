4fd43961938f8432889a50e73a208406f11c0dee
Jorge Villalobos <mail@minusfive.com>
2024-12-29 00:40:50 -0500

    fix(nvim): remove groovyls due to mason errors

--- .config/nvim/lua/plugins/lang/groovy.lua
+++ .config/nvim/lua/plugins/lang/groovy.lua
@@ -1,11 +1,11 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@module 'lspconfig'
-      ---@type {[string]: lspconfig.Config|{}}
-      servers = { groovyls = {} },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   ---@class PluginLspOpts
+  --   opts = {
+  --     ---@module 'lspconfig'
+  --     ---@type {[string]: lspconfig.Config|{}}
+  --     servers = { groovyls = {} },
+  --   },
+  -- },
 }
