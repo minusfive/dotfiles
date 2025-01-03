9d0da248fc472b8920345ea6399ca114443b0ec0
Jorge Villalobos <mail@minusfive.com>
2025-01-03 12:17:27 -0500

    feat(1Password): added 1P cli

--- nix/programs/1password/default.nix
+++ nix/programs/1password/default.nix
@@ -1,8 +1,13 @@
+{ pkgs, user, ... }:
 {
   homebrew = {
     casks = [ "1password" ];
     masApps = {
       "1Password for Safari" = 1569813296;
     };
+  };
+
+  home-manager.users.${user} = {
+    home.packages = [ pkgs._1password-cli ];
   };
 }
