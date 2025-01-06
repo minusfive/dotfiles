f30059eff54552d64e9d64380d866785369486d0
Jorge Villalobos <mail@minusfive.com>
2025-01-05 22:12:36 -0500

    feat(hs): appLauncher option to send notifications on activation

--- .config/hammerspoon/modules/appLauncher.lua
+++ .config/hammerspoon/modules/appLauncher.lua
@@ -1,14 +1,37 @@
 -- Module to open / focus applictions and automate application tasks
 ---@class AppLauncher
-local AppLauncher = {}
+---@field notifyOnActivation? boolean Whether to send notifications on app activation
+local AppLauncher = { notifyOnActivation = false }
 
 -- Quickly open and/or focus applications
 ---@param name string
 ---@return function
 function AppLauncher:openApp(name)
-	return function()
-		hs.application.launchOrFocus(name)
-	end
+  return function()
+    hs.application.launchOrFocus(name)
+  end
+end
+
+-- Sends a system notification whenever an app is activated
+---@param app hs.application
+local function __sendAppActivationNotification(app)
+  if not AppLauncher.notifyOnActivation then
+    return
+  end
+
+  local bundleID = app:bundleID()
+  local appIcon = bundleID and hs.image.imageFromAppBundle(bundleID)
+
+  local notification = hs.notify.new(nil, {
+    title = app:name(),
+    subTitle = "App Activated",
+    information = app:pid(),
+    contentImage = appIcon,
+  })
+
+  if notification then
+    notification:send()
+  end
 end
 
 -- Watch applications and automate actions
@@ -16,11 +39,13 @@ end
 ---@param eventType unknown
 ---@param app hs.application
 local function appWatcher(name, eventType, app)
-	if eventType == hs.application.watcher.activated then
-		if name == "Finder" then
-			app:selectMenuItem({ "Window", "Bring All to Front" })
-		end
-	end
+  if eventType == hs.application.watcher.activated then
+    if name == "Finder" then
+      app:selectMenuItem({ "Window", "Bring All to Front" })
+    end
+
+    __sendAppActivationNotification(app)
+  end
 end
 
 AppLauncher.watcher = hs.application.watcher.new(appWatcher)
