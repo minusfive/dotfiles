1668fee26cebca94138ae2e564491f6d8073e1ed
Jorge Villalobos <mail@minusfive.com>
2025-03-18 15:05:37 -0400

    feat(hs): caffeine persists state on reload

--- .config/hammerspoon/Spoons/Caffeine.spoon/init.lua
+++ .config/hammerspoon/Spoons/Caffeine.spoon/init.lua
@@ -5,7 +5,7 @@
 ---@field watcher? hs.caffeinate.watcher
 local Caffeine = {
   name = "Caffeine",
-  version = "1.0",
+  version = "0.1",
   author = "http://github.com/minusfive",
   license = "MIT - https://opensource.org/licenses/MIT",
 
@@ -16,53 +16,61 @@ local Caffeine = {
   },
 }
 
--- Gets the caffeine active state
----@return boolean | nil
-local function __getCaffeineState()
-  return hs.caffeinate.get("displayIdle")
-end
+-- Constants
+local __sleepType = "displayIdle"
+local __stateKey = "caffeine"
 
 -- Gets the menuIcon from state
 ---@param isActive? boolean
-local function __getMenuIcon(isActive)
-  return isActive and "0.0" or "-.-"
-end
+local function __getMenuIcon(isActive) return isActive and "0.0" or "-.-" end
 
 --- Gets the tooltip from state
 ---@param isActive? boolean
-local function __getMenuTooltip(isActive)
-  return isActive and "Insomniac" or "Sleepy"
-end
+local function __getMenuTooltip(isActive) return isActive and "Caffeinated" or "Sleepy" end
 
 -- Sends a system notification whenever an app is activated
 ---@param isActive? boolean
 local function __sendAppActivationNotification(isActive)
-  if not Caffeine.options.notifyOnStateChange then
-    return
-  end
+  if not Caffeine.options.notifyOnStateChange then return end
 
   local notification = hs.notify.new(nil, {
     title = __getMenuIcon(isActive),
     subTitle = __getMenuTooltip(isActive),
   })
 
-  if notification then
-    notification:send()
-  end
+  if notification then notification:send() end
 end
 
+-- Persists state in OS defaults table
+---@param isActive? boolean
+local function __persistState(isActive) hs.settings.set(__stateKey, isActive) end
+
+-- Reads state from OS defaults table
+local function __readPersistedState() return hs.settings.get(__stateKey) end
+
 -- Toggle menuBarItem
-local function __toggleMenuBarItem()
-  local isActive = __getCaffeineState()
+---@param isActive? boolean
+local function __toggleMenuBarItem(isActive)
   Caffeine.menuBarItem:setTitle(__getMenuIcon(isActive))
   Caffeine.menuBarItem:setTooltip(__getMenuTooltip(isActive))
-  __sendAppActivationNotification(isActive)
 end
 
 -- Toggles system caffeination
-function Caffeine.toggle()
-  hs.caffeinate.toggle("displayIdle")
-  __toggleMenuBarItem()
+---@param shouldActivate? boolean
+function Caffeine.toggle(shouldActivate)
+  ---@type boolean | nil
+  local isActive = false
+
+  if shouldActivate == nil then
+    isActive = hs.caffeinate.toggle(__sleepType)
+  else
+    hs.caffeinate.set(__sleepType, shouldActivate)
+    isActive = shouldActivate
+  end
+
+  __persistState(isActive)
+  __toggleMenuBarItem(isActive)
+  __sendAppActivationNotification(isActive)
 end
 
 function Caffeine:start()
@@ -70,13 +78,11 @@ function Caffeine:start()
     Caffeine.menuBarItem = hs.menubar.new(true, "hs.minusfive.caffeine")
     Caffeine.menuBarItem:setClickCallback(Caffeine.toggle)
   end
-  __toggleMenuBarItem()
+  Caffeine.toggle(__readPersistedState() or false)
 end
 
 function Caffeine:stop()
-  if Caffeine.menuBarItem then
-    Caffeine.menuBarItem:delete()
-  end
+  if Caffeine.menuBarItem then Caffeine.menuBarItem:delete() end
 end
 
 return Caffeine
