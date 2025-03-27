dedbf0e0c181448b7f95cf4fcd38bb5921abcc00
Jorge Villalobos <mail@minusfive.com>
2025-03-27 15:49:52 -0400

    fix(nvim): copilot.lua broken; updates

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -23,6 +23,9 @@ local function get_indexed_keymap()
 end
 
 return {
+  -- Copilot Broken: https://github.com/zbirenbaum/copilot.lua/issues/408#issuecomment-2758411881
+  { "zbirenbaum/copilot.lua", commit = "99654fe9ad6cb2500c66b178a03326f75c95f176" },
+
   {
     "saghen/blink.cmp",
     optional = true,
